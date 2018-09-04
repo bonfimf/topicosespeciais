2fd5aa9a554f0c3e1558f66dc2390dac66d93550
bonfimf <bonfimadonias@gmail.com>
2018-09-04 20:08:05 -0300

    aulajpa2

--- src/fvs/edu/aplicacao/Principal.java
+++ src/fvs/edu/aplicacao/Principal.java
@@ -4,14 +4,21 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import fvs.edu.dominio.Aluno;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("topicos-fvs");
 		
 		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
 		
-
+   Aluno aluno1 = new Aluno
+       (null, "Bonfimf", "123", "123", 5671);  
+   em.persist(aluno1);
+   em.getTransaction().commit();
+   System.out.println("salvo com sucesso");
 	}
 
 }
