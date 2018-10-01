2ab53db59e415212b0b9b120631fad609b459484
bonfimf <bonfimadonias@gmail.com>
2018-10-01 20:03:17 -0300

    Implementação do Domínio Categoria

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Categoria.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Categoria.java
@@ -1,11 +1,16 @@
 package fvs.edu.br.topicos.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+
 
 @Entity
 
@@ -17,6 +22,9 @@ public class Categoria  implements Serializable{
  @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany
+	List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
---
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Produto.java
@@ -0,0 +1,13 @@
+package fvs.edu.br.topicos.domain;
+
+public class Produto {
+
+	private Integer id;
+	private String nome;
+	private Double preco; 
+	
+	
+	
+	}
+
+
