2208588daff5ac1695c9ac0ffbd663e6191ae221
bonfimf <42556688+bonfimf@users.noreply.github.com>
2018-10-02 20:05:22 -0300

    Finalização da classe produto
    Finalização da classe produto

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Categoria.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Categoria.java
@@ -10,25 +10,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-
-
 @Entity
-
-
-public class Categoria  implements Serializable{
- private static final long serialVersionUID = 1L;
- 
- @Id
- @GeneratedValue(strategy=GenerationType.IDENTITY)
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	@ManyToMany
+	@ManyToMany(mappedBy="categorias")
 	List<Produto> produtos = new ArrayList<>();
 	
-	public Categoria() {
+	public Categoria( ) {
 		
 	}
+
 
 	public Categoria(Integer id, String nome) {
 		super();
@@ -36,21 +33,37 @@ public class Categoria  implements Serializable{
 		this.nome = nome;
 	}
 
+
 	public Integer getId() {
 		return id;
 	}
+
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+
 	public String getNome() {
 		return nome;
 	}
 
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	
 
 	@Override
 	public int hashCode() {
@@ -59,6 +72,7 @@ public class Categoria  implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -78,6 +92,5 @@ public class Categoria  implements Serializable{
 	}
 	
 	
-	}
-
-
+	
+}
